7b6a7c6689ef2e1dcc7b1f8c46142566564a4a4b
CyberYurich <alexei_artemov@mail.ru>
2015-10-15 14:45:12 +0400

    add entry.java

---
+++ src/java/com/model/Entry.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ALEX
+ */
+public class Entry {
+    private int id;
+    private Date date;
+    private int placeInQueue;
+    private String lastname;
+    private String firstname;
+    private String middlename;
+    private long phone;
+    private String email;
+    private int shoeSize;
+    private String productModel;
+    
+    public Entry() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getPlaceInQueue() {
+        return placeInQueue;
+    }
+
+    public void setPlaceInQueue(int placeInQueue) {
+        this.placeInQueue = placeInQueue;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public void setMiddlename(String middlename) {
+        this.middlename = middlename;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getShoeSize() {
+        return shoeSize;
+    }
+
+    public void setShoeSize(int shoeSize) {
+        this.shoeSize = shoeSize;
+    }
+
+    public String getProductModel() {
+        return productModel;
+    }
+
+    public void setProductModel(String productModel) {
+        this.productModel = productModel;
+    }
+}
