f8764eb07f3fd1ab0600080d3204f17a741c6143
CyberYurich <alexei_artemov@mail.ru>
2015-10-29 22:00:55 +0400

    move jsp in WEB-INF

--- src/java/com/controller/servlets/ShowAllEntries.java
+++ src/java/com/controller/servlets/ShowAllEntries.java
@@ -34,7 +34,7 @@ public class ShowAllEntries extends HttpServlet {
             throws ServletException, IOException {
         
         request.setAttribute("entries", MysqlDaoSingleton.getInstance().readAll());
-        request.getRequestDispatcher("show-all-entries.jsp").forward(request, response);
+        request.getRequestDispatcher("WEB-INF/show-all-entries.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
